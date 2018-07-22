3a20a84ccd1af48a1c8dbfd01e29a7bdc24c0d73
Code Hedgehog <codehedgehog@users.noreply.github.com>
2018-07-22 15:33:58 -0700

    Update Api project to use Microsoft.AspNetCore.App and Microsoft.AspNetCore.Mvc

--- Quickstarts/2_ResourceOwnerPasswords/src/QuickstartIdentityServer/Program.cs
+++ Quickstarts/2_ResourceOwnerPasswords/src/QuickstartIdentityServer/Program.cs
@@ -7,18 +7,18 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace QuickstartIdentityServer
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Console.Title = "IdentityServer";
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Title = "IdentityServer";
 
-            BuildWebHost(args).Run();
-        }
+			BuildWebHost(args).Run();
+		}
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-    }
+		public static IWebHost BuildWebHost(string[] args) =>
+				WebHost.CreateDefaultBuilder(args)
+						.UseStartup<Startup>()
+						.Build();
+	}
 }
