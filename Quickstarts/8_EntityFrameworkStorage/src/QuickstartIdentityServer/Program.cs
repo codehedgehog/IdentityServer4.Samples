69776790c3cfec29e6d2828a578cece07c4e532f
CodeHedgehog <codehedgehog@outlook.com>
2018-07-23 19:33:22 -0700

    Modify EF Core migrations default schema

--- Quickstarts/8_EntityFrameworkStorage/src/QuickstartIdentityServer/Program.cs
+++ Quickstarts/8_EntityFrameworkStorage/src/QuickstartIdentityServer/Program.cs
@@ -8,31 +8,31 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace QuickstartIdentityServer
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Console.Title = "IdentityServer";
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Title = "IdentityServer";
 
-            var seed = args.Contains("/seed");
-            if (seed)
-            {
-                args = args.Except(new[] { "/seed" }).ToArray();
-            }
+			var seed = args.Contains("/seed");
+			if (seed)
+			{
+				args = args.Except(new[] { "/seed" }).ToArray();
+			}
 
-            var host = BuildWebHost(args);
+			var host = BuildWebHost(args);
 
-            if (seed)
-            {
-                SeedData.EnsureSeedData(host.Services);
-            }
+			if (seed)
+			{
+				SeedData.EnsureSeedData(host.Services);
+			}
 
-            host.Run();
-        }
+			host.Run();
+		}
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-    }
+		public static IWebHost BuildWebHost(string[] args) =>
+				WebHost.CreateDefaultBuilder(args)
+						.UseStartup<Startup>()
+						.Build();
+	}
 }
--- Quickstarts/8_EntityFrameworkStorage/src/QuickstartIdentityServer/Startup.cs
+++ Quickstarts/8_EntityFrameworkStorage/src/QuickstartIdentityServer/Startup.cs
@@ -1,83 +1,126 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-using System.Reflection;
-using IdentityServer4;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
 
 namespace QuickstartIdentityServer
 {
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
+	using IdentityServer4;
+	using IdentityServer4.EntityFramework.DbContexts;
+	using IdentityServer4.EntityFramework.Mappers;
+	using Microsoft.AspNetCore.Builder;
+	using Microsoft.AspNetCore.Hosting;
+	using Microsoft.EntityFrameworkCore;
+	using Microsoft.EntityFrameworkCore.Internal;
+	using Microsoft.Extensions.DependencyInjection;
+	using Microsoft.IdentityModel.Tokens;
+	using System.Reflection;
+	public class Startup
+	{
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
 
-            const string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;database=IdentityServer4.Quickstart.EntityFramework-2.0.0;trusted_connection=yes;";
-            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+			const string connectionString = @"Data Source=ITDEVSQL;database=WIRAWANTEST;trusted_connection=yes;";
+			var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
 
-            // configure identity server with in-memory stores, keys, clients and scopes
-            services.AddIdentityServer()
-                .AddDeveloperSigningCredential()
-                .AddTestUsers(Config.GetUsers())
-                // this adds the config data from DB (clients, resources)
-                .AddConfigurationStore(options =>
-                {
-                    options.ConfigureDbContext = builder =>
-                        builder.UseSqlServer(connectionString,
-                            sql => sql.MigrationsAssembly(migrationsAssembly));
-                })
-                // this adds the operational data from DB (codes, tokens, consents)
-                .AddOperationalStore(options =>
-                {
-                    options.ConfigureDbContext = builder =>
-                        builder.UseSqlServer(connectionString,
-                            sql => sql.MigrationsAssembly(migrationsAssembly));
+			// configure identity server with in-memory stores, keys, clients and scopes
+			services.AddIdentityServer()
+					.AddDeveloperSigningCredential()
+					.AddTestUsers(Config.GetUsers())
+					// this adds the config data from DB (clients, resources)
+					.AddConfigurationStore(options =>
+					{
+						options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+						options.DefaultSchema = "idsrv";
+					})
+					// this adds the operational data from DB (codes, tokens, consents)
+					.AddOperationalStore(options =>
+					{
+						options.ConfigureDbContext = builder =>
+											builder.UseSqlServer(connectionString,
+													sql => sql.MigrationsAssembly(migrationsAssembly));
 
-                    // this enables automatic token cleanup. this is optional.
-                    options.EnableTokenCleanup = true;
-                    options.TokenCleanupInterval = 30;
-                });
+									// this enables automatic token cleanup. this is optional.
+									options.EnableTokenCleanup = true;
+						options.TokenCleanupInterval = 30;
+						options.DefaultSchema = "idsrv";
+					});
 
-            services.AddAuthentication()
-                .AddGoogle("Google", options =>
-                {
-                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+			services.AddAuthentication()
+					.AddGoogle("Google", options =>
+					{
+						options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
 
-                    options.ClientId = "434483408261-55tc8n0cs4ff1fe21ea8df2o443v2iuc.apps.googleusercontent.com";
-                    options.ClientSecret = "3gcoTrEDPPJ0ukn_aYYT6PWo";
-                })
-                .AddOpenIdConnect("oidc", "OpenID Connect", options =>
-                {
-                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-                    options.SignOutScheme = IdentityServerConstants.SignoutScheme;
+						options.ClientId = "434483408261-55tc8n0cs4ff1fe21ea8df2o443v2iuc.apps.googleusercontent.com";
+						options.ClientSecret = "3gcoTrEDPPJ0ukn_aYYT6PWo";
+					})
+					.AddOpenIdConnect("oidc", "OpenID Connect", options =>
+					{
+						options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+						options.SignOutScheme = IdentityServerConstants.SignoutScheme;
 
-                    options.Authority = "https://demo.identityserver.io/";
-                    options.ClientId = "implicit";
+						options.Authority = "https://demo.identityserver.io/";
+						options.ClientId = "implicit";
 
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        NameClaimType = "name",
-                        RoleClaimType = "role"
-                    };
-                });
-        }
+						options.TokenValidationParameters = new TokenValidationParameters
+						{
+							NameClaimType = "name",
+							RoleClaimType = "role"
+						};
+					});
+		}
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			InitializeDatabase(app);
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
 
-            app.UseIdentityServer();
+			}
 
-            app.UseStaticFiles();
-            app.UseMvcWithDefaultRoute();
-        }
-    }
+			app.UseIdentityServer();
+
+			app.UseStaticFiles();
+			app.UseMvcWithDefaultRoute();
+		}
+
+		private void InitializeDatabase(IApplicationBuilder app)
+		{
+			using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+			{
+				serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+				var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+				context.Database.Migrate();
+				if (!context.Clients.Any())
+				{
+					foreach (var client in Config.GetClients())
+					{
+						context.Clients.Add(client.ToEntity());
+					}
+					context.SaveChanges();
+				}
+
+				if (!context.IdentityResources.Any())
+				{
+					foreach (var resource in Config.GetIdentityResources())
+					{
+						context.IdentityResources.Add(resource.ToEntity());
+					}
+					context.SaveChanges();
+				}
+
+				if (!context.ApiResources.Any())
+				{
+					foreach (var resource in Config.GetApiResources())
+					{
+						context.ApiResources.Add(resource.ToEntity());
+					}
+					context.SaveChanges();
+				}
+			}
+		}
+
+	}
 }
